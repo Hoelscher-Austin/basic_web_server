6c7864a5c8795e99dcb8909d21efeeb2167abab2
Austin Hoelscher <Hoelscher.Austin@yahoo.com>
2025-05-01 15:51:43 -0500

    Initial Commit Finished Project

---
+++ dir.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+if(!fs.existsSync('./new')){
+    fs.mkdir('./new', (err) => {
+        if (err) throw err;
+        console.log('Directory Created.');
+    });
+}
+else{
+    console.log('Directory Already Exists...');
+}
+
+if(fs.existsSync('./new')){
+    fs.rmdir('./new', (err) => {
+        if (err) throw err;
+        console.log('Directory Deleted.');
+    });
+}
+
